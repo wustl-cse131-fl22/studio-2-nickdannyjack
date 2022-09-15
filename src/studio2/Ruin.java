fc298b9820d175e9b6e716f28aaee8f7c511bf72
RandyDi <d.randy@wustl.edu>
2022-09-15 14:14:56 -0500

    V 1.2.0

---
+++ src/studio2/Ruin.java
@@ -0,0 +1,60 @@
+package studio2;
+
+import java.util.Scanner;
+
+public class Ruin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+		System.out.println("Start amount:");
+		int startAmount = in.nextInt();
+		System.out.println("Win chance: ");
+		double winChance = in.nextDouble();
+		System.out.println("What is your win limit?");
+		int winLimit = in.nextInt();
+		System.out.println("How many Simulations");
+		int totalSimulation = in.nextInt();
+
+		int totalLoses = 0;
+
+		int counterSimulation = 0;
+	
+
+		
+		while(counterSimulation <= totalSimulation) {
+			
+			int counter = 0; 
+						
+			while((startAmount < winLimit) && (0 <= startAmount)) {
+				boolean chance = Math.random() <= winChance;
+				if(chance == true) {
+					startAmount++;
+				}
+				else {
+					startAmount--;
+					totalLoses++;
+				}
+				counter++;
+				//End of Second While
+			}
+			
+			if(startAmount == winLimit) {
+				System.out.println("Simulation " + counterSimulation +": " + counter + " Win");	
+				counter = 0;
+			}
+			else {
+				System.out.println("Simulation " + counterSimulation +": " + counter + " Lose");
+				counter = 0;
+			}
+			//End of First While
+			//System.out.println("Simulations ran: " + counter);
+			counterSimulation++;
+			
+		}
+		double ruinRate = totalLoses  / totalSimulation;
+
+
+	}
+}
